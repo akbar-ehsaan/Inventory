2865a2605678a014970cb5b81d63afff1b1608cf
e.akbar <e.akbar@emofid.com>
2024-08-09 12:47:54 +0330

    adding exception middleware forerror handling

---
+++ inventory.api/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,27 @@
+﻿public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (ArgumentException ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            await context.Response.WriteAsJsonAsync(new { Message = ex.Message });
+        }
+        catch (Exception ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            await context.Response.WriteAsJsonAsync(new { Message = "An unexpected error occurred.", Details = ex.Message });
+        }
+    }
+}
